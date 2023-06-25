adf467131e91c4bdeecd352d7a4d9c563b06058c
Mojaer Ahmed <mojaermohi@gmail.com>
2023-06-26 02:26:57 +0600

    mail is sent successfully

---
+++ index.js
@@ -0,0 +1,52 @@
+const nodemailer = require("nodemailer");
+const { google } = require('googleapis');
+require('dotenv').config()
+
+
+const clientId = process.env.YOUR_CLIENT_ID
+const clientSecret = process.env.YOUR_CLIENT_SECRET
+const redirectUrl = process.env.YOUR_REDIRECT_URL
+const refreshToken = process.env.REFRESH_TOKEN
+
+const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUrl)
+oAuth2Client.setCredentials({
+    refresh_token: refreshToken
+});
+
+const sendMail = async () => {
+
+    try {
+        const { token } = await oAuth2Client.getAccessToken()
+        console.log(token)
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'oAuth2',
+                user: process.env.USER,
+                clientId: clientId,
+                clientSecret: clientSecret,
+                refresh_token: refreshToken,
+                accessToken: token
+            }
+        }
+        )
+
+        const mail = {
+            from: 'Hello 🙌 <mojaermohi2@gmail.com>',
+            to: 'mojaermohi1@gmail.com',
+            subject: 'test subject',
+            text: 'success message',
+            html: '<h1>hello brothers </h1>'
+        }
+
+        const result = await transporter.sendMail(mail)
+        return result
+
+    } catch (error) {
+        return error
+    }
+
+
+}
+sendMail().then(result => console.log('success', result)).catch(error => console.log('error', error.message));
